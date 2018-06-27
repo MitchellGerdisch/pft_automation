a35f29d28eaf2c5b240c76e66d1b78c31c99eec5
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2018-06-27 06:30:15 -0700

    updates

--- PFT_automation/User_Management/create_user.sh
+++ PFT_automation/User_Management/create_user.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-if [ $# -ne 7 ]
+if [ $# -ne 6 ]
 then
-	echo "USAGE: $0 ACCOUNT_NUMBER firstName lastName companyName emailAddress phoneNumber password"
+	#echo "USAGE: $0 ACCOUNT_NUMBER firstName lastName companyName emailAddress phoneNumber password"
+	echo "USAGE: $0 firstName lastName companyName emailAddress phoneNumber password"
 	echo "Where ACCOUNT_NUMBER is the account number to which to add the users."
 	echo "And the user information is the user's"
 	echo "   FirstName LastName CompanyName EmailAddress PhoneNumber Password"
 	exit 1
 fi
 
-ACCOUNT_NUMBER=${1}
-FirstName=${2} 
-LastName=${3} 
-CompanyName=${4} 
-EmailAddress=${5} 
-PhoneNumber=${6} 
-Password=${7}
+#ACCOUNT_NUMBER=${1}
+FirstName=${1} 
+LastName=${2} 
+CompanyName=${3} 
+EmailAddress=${4} 
+PhoneNumber=${5} 
+Password=${6}
 
 # Check to see that the rsc tool is installed.
 rsc --version > /dev/null
@@ -26,7 +27,8 @@ then
 	exit 1
 fi
 
-user_href=`rsc --pp -a ${ACCOUNT_NUMBER} --xh=Location cm15 create /api/users "user[first_name]=${FirstName}" "user[last_name]=${LastName}" "user[company]=${CompanyName}" "user[email]=${EmailAddress}" "user[phone]=${PhoneNumber}" "user[password]=${Password}"`
+#user_href=`rsc --pp -a ${ACCOUNT_NUMBER} --xh=Location cm15 create /api/users "user[first_name]=${FirstName}" "user[last_name]=${LastName}" "user[company]=${CompanyName}" "user[email]=${EmailAddress}" "user[phone]=${PhoneNumber}" "user[password]=${Password}"`
+user_href=`rsc --pp --xh=Location cm15 create /api/users "user[first_name]=${FirstName}" "user[last_name]=${LastName}" "user[company]=${CompanyName}" "user[email]=${EmailAddress}" "user[phone]=${PhoneNumber}" "user[password]=${Password}"`
 
 printf "${user_href}"
 
